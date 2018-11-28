50ef5b1e9ed6452367f8f00d6768e24562f3266f
tthttl@gmail.com <tthttl@gmail.com>
2018-11-28 20:26:17 +0100

    initialize

---
+++ src/main/java/com/tthttl/srpingrecipe/SrpingrecipeApplication.java
@@ -0,0 +1,12 @@
+package com.tthttl.srpingrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingrecipeApplication.class, args);
+	}
+}
